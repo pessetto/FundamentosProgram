3ffdecaabc9c53c76c30a5c766593ef936f0f014
Júlia Pessetto <84971505+pessetto@users.noreply.github.com>
2024-04-25 21:11:41 -0300

    Create lista5.py

---
+++ lista5.py
@@ -0,0 +1,37 @@
+1. Implemente uma função cumprimentar que receba como parâmetro o nome de uma pessoa e escreva
+a mensagem “Olá, <nome_da_pessoa>!”
+2. Implemente uma função tabuada que receba como parâmetro um número inteiro e escreva a tabuada
+desse número.
+3. Implemente uma função mediaUnisinos que receba como parâmetro as notas do Grau A e do Grau B e
+retorne o resultado do grau final.
+4. Implemente uma função sorteio que receba o intervalo de valores inteiros início e fim como parâmetro
+e sorteie um número dentro do intervalo (considerando intervalo fechado [início, fim])
+5. Utilizando o template de menus mostrado em aula, faça um programa que simule uma calculadora
+simples, mostrando ao usuário as opções somar, subtrair, multiplicar e dividir dois números reais. Cada
+uma das operações deve ser executada em funções que recebam como parâmetro os dois números,
+lidos do usuário. As funções devem retornar o resultado para o programa principal, que o exibirá na
+tela. OBS.: Apenas chame a função de dividir se o divisor for diferente de zero (divisão por zero não
+existe!).
+6. Utilizando o template de menus mostrado em aula, faça um programa que simule um sistema de lootbox
+simples. As opções para o usuário são abrir uma caixa e consultar itens coletados. Ao abrir uma caixa, o
+usuário irá receber 1 item comum, raro ou lendário, conforme a probabilidade da tabela abaixo:
+Tipo de item Probabilidade de obtenção
+Comum 80 %
+Raro 19%
+Lendário 1%
+Ao obter o item, deverá se atualizar o inventário do jogador contabilizando o número de itens comuns,
+raros e lendários que ele possui. Ao consultar os itens coletados, deverá ser mostrado ao usuário a
+quantidade de itens de cada tipo que ele possui.
+Exemplos de tela:
+1 – Abrir uma caixa
+2 – Consultar itens
+0 - Sair
+Escolha uma opção: 1
+Você coletou 1 item comum!
+1 – Abrir uma caixa
+2 – Consultar itens
+0 - Sair
+Escolha uma opção: 2
+Itens comuns: 9
+Itens raros: 2
+Itens lendários: 0
