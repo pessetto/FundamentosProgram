a2d98d3f63955086dcfdc6e8ad0a451c07100f9c
Júlia Pessetto <84971505+pessetto@users.noreply.github.com>
2024-04-25 21:10:40 -0300

    Create lista4.py

---
+++ lista4.py
@@ -0,0 +1,59 @@
+1. Faça um algoritmo para:
+a. Gerar e escrever todos os números inteiros do intervalo [0,100].
+b. Gerar e escrever os números pares do intervalo [20,50].
+c. Gerar e escrever os números inteiros do intervalo [25,70] em ordem decrescente.
+d. Gerar e escrever os números ímpares do intervalo [25,95] em ordem decrescente.
+e. Ler 15 números e escrever a soma e a média dos números lidos.
+f. Ler 10 números inteiros e escrever a quantidade de números pares e a quantidade de números
+ímpares lidos.
+g. Sortear 20 números inteiros entre -10 e 10 e imprimi-los acompanhados da mensagem
+“POSITIVO”, “NEGATIVO”, ou “NULO”, conforme o caso. No final, imprimir a quantidade de
+números positivos e negativos lidos.
+h. Ler n números e imprimir no final a soma dos números lidos (obs.: n é a quantidade de números
+que deverão ser lidos e também deve ser lido do teclado)
+2. Implemente um programa que sorteia um número de 1 a 10 e dá ao usuário 3 tentativas de acertá-lo.
+A cada tentativa errada, o programa informa se o número a adivinhar está abaixo ou acima.
+3. Elabore um programa que lê um número de 1 a 9 e mostra a tabuada de multiplicação do número.
+Exemplo de tela de saída:
+Entre com um número: 5
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+Calcular outro número (s/n)? n
+4. Escrever um programa que calcule todos os números divisíveis por certo valor indicado pelo usuário (o
+resto da divisão por este número deve ser igual a zero), compreendidos em um intervalo também
+especificado pelo usuário. O usuário deve entrar com um primeiro valor correspondente ao divisor e
+após ele vai fornecer o valor inicial do intervalo, seguido do valor final deste intervalo.
+ Exemplo de tela de saída:
+Entre com o valor do divisor: 3
+Início do intervalo: 17
+Final do intervalo: 29
+Números divisíveis por 3 no intervalo de 17 a 29:
+18 21 24 27
+5. Para x alunos da Unisinos, ler as notas do grau A e grau B e calcular a média considerando o sistema
+de notas da Unisinos. Se o aluno estiver aprovado escrever “APROVADO”. Caso contrário, ler o grau C
+e pedir qual o grau que deve ser substituído (A ou B, maiúsculo ou minúsculo), recalcular a média. Se
+estiver aprovado, escrever “APROVADO”, caso contrário escrever “REPROVADO”. No final escrever a
+média geral dos alunos.
+6. Crie um algoritmo que sorteie 5 valores entre 0 e 100 e depois imprima o menor, o maior e a média
+dos valores sorteados.
+7. Descubra, dentre cinco nomes informados pelo usuário, qual o primeiro em ordem alfabética.
+8. Fazer um programa que calcule e imprima o fatorial de um número fornecido pelo usuário. Repetir a
+execução do programa tantas até o usuário responder não. O fatorial de um número inteiro positivo é
+definido como o número multiplicado por ele menos 1, menos 2, etc até o valor 1.
+Por exemplo, o fatorial de 4 é 4x3x2x1=24.
+Exemplo de tela de saída:
+Entre com um número: 5
+O fatorial de 5 é 120
+Calcular outro número (s/n)? n
+9. Escrever um programa que produza a saída abaixo na tela, para n linhas e usando um caractere lido do
+teclado.
+Exemplo de tela de saída, para n = 5 e caracter = `*`:
+Entre com um número: 5
+Entre com um caracter: *
+*
+* *
+* * *
+* * * *
+* * * * *
